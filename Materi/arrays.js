be6abb69e4a6171bfcc53fcc69e343a942fbbbae
Catur Satrio <52611164+Wong801@users.noreply.github.com>
2020-11-18 21:20:30 +0700

    Update arrays.js

--- Materi/arrays.js
+++ Materi/arrays.js
@@ -21,3 +21,44 @@ console.log(number.length); //length juga akan berubah
 
 number.pop(); //digunakan untuk melepas data terakhir dari array yang kita inginkan
 console.log(number)
+
+//Pertemuan ke 4
+
+console.log('Hari ke 4');
+
+let bb = [2, 35, 76, 90, 32, 21];
+let genap = [];
+let ganjil = [];
+
+for(let i = 0; i < bb.length; i++) {
+    if(bb[i] % 2 == 0) {
+        genap.push(bb[i]);
+    } else {
+        ganjil.push(bb[i]);
+    }
+}
+
+console.log("bilangan genap : " + genap);
+console.log("bilangan ganjil : " + ganjil);
+
+let soal_2 = ["aa", "bbb", "cccc", "ddddd"];
+
+for(let i = 0; i < soal_2.length; i++) {
+    if(soal_2[i].length > 3) {
+        console.log(soal_2[i]);
+    }
+}
+
+for(let i = 0; i < 10; i++) {
+    for(let j = 0; j < 10; j++) {
+        if(i % 2 == 0) {
+            document.write("*");
+        } else {
+            document.write("_");
+        }
+    }
+    document.write("<br>")
+}
+
+// * menempati angka genap dari deklarasi i
+// _ menempati angka genap dari deklarasi i
